7f4a2ef2fa6b8604ae61535e9e16faf81a513fea
Phil Fenstermacher <phillip.fenstermacher@gmail.com>
2015-10-07 11:38:06 -0700

    Fix tests after module rename

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,4 +1,4 @@
-class cloudwatch::package {
+class cloudwatch_agent::package {
   if $cloudwatch_agent::package_install {
     package { 'awslogs':
       ensure => $cloudwatch_agent::package_ensure,
@@ -16,8 +16,8 @@ class cloudwatch::package {
     }
 
     exec { 'install-awslogs':
-      command => "env && curl ${installer_url} |\
-        python - -r ${region} -n -c /tmp/aws.conf",
+      command => "env && curl ${cloudwatch_agent::installer_url} |\
+        python - -r ${cloudwatch_agent::region} -n -c /tmp/aws.conf",
       creates => '/var/awslogs/etc/aws.conf',
       cwd     => '/tmp',
       path    => ['/usr/bin', '/usr/local/sbin', '/usr/sbin', '/sbin', '/bin'],
